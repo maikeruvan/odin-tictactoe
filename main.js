61974e8c1d69fb6be6eff3293c8c733d0fa42905
maikeruban <vanmichael.v8@gmail.com>
2026-09-08 21:55:47 +0800

    Still trying to build the model after a long rest

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ tie if all boards are filled but no winner
 
 // IIFE -> single object - MODEL
 const gameBoard = (() => {
-    const board = [];
+    let board = [];
 
     // Builds the 2D array as the board
     for (let row = 0; row < 3; row++) {
         board.push([]);
         for (let col = 0; col < 3; col++) {
-            board[row].push(col);
+            board[row].push('');
         }
     }
 
@@ -28,11 +28,9 @@ const gameBoard = (() => {
         return board;
     };
 
-    // Marks the board with the Player object
-    const markBoard = (player) => {
-        // player.getName
-        // player.getMarker -> X or O
-        // player.getMove -> [row][col]
+    // markPosition is an object mapping row and col value to the board
+    const markBoard = (playerMark, markPosition) => {
+        board[markPosition.row][markPosition.col] = playerMark;
     };
 
     const clearBoard = () => {
@@ -49,15 +47,31 @@ const gameBoard = (() => {
 
 // IIFE -> Single object -> CONTROLLER
 const gameController = (() => {
-    let gameStarted = false;
+    let gameOver = true;
+    let activePlayer = null;
 
     // Starts the game
     const start = () => {
-        // Clears the board first gameBoard.clearBoard()
+        const playerX = createPlayer('Player X', 'X');
+        const playerO = createPlayer('Player O', 'O');
+        
+        gameOver = false;
+        activePlayer = playerO;
+        gameBoard.clearBoard();
+
         // Builds the board model into the view updateBoardUI
-        // gameStarted = true;
+
         // Adds click listener to the board or to every column addListener()
-        // When a click happens, a move was made, call getMove
+        // When a click happens, a move was made, call setPlayerMove
+        // also switchPlayer()
+    };
+
+    // Private factory function
+    const createPlayer = (name, marker) => {
+        return {
+            name,
+            marker
+        };
     };
 
     const addListener = () => {
@@ -69,34 +83,62 @@ const gameController = (() => {
     };
 
     // Called when a click happens
-    const getMove = (player, position) => {
-        // returns the player who made the move
-        // returns the position marked by the player [row][col]
+    const setPlayerMove = () => {
+        // finds the position marked by the player [row][col]
+        // set markPosition to have row and col as the position of the move
+        // gameBoard.markBoard(activePlayer.marker, markPosition);
+        // switchPlayer() -> switch the next activePlayer
+    };
+
+    // Called when a click happens
+    const switchPlayer = () => {
+        switch (activePlayer) {
+            case playerO:
+                activePlayer = playerX;
+                break;
+
+            case playerX:
+                activePlayer = playerO;
+                break;
+        }
+    };
+
+    // Returns the winning player and winning positions
+    const getWinner = () => {
+        const board = gameBoard.getBoard();
+        const winPositions = [
+            [[0]],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ];
     };
 
     // Reset the game
     const restart = () => {
-        gameStarted = false;
+        gameOver = true;
+        activePlayer = null;
         removeListener();
         start();
     };
 
     return {
         start,
-        getMove,
+        addListener,
+        removeListener,
+        getWinner,
+        setPlayerMove,
         restart
     };
 })();
 
-// Factory Function returning player object
-function createPlayer(name, marker) {
-    return {
-        name,
-        marker
-    };
-}
+console.log(gameController.getWinner());
 
-function updateBoardUI() {
+function updateBoardUI(boardState) {
     // renders the UI
     // takes the game board model and render as the view
 }
